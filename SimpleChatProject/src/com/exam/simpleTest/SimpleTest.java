16d948fb30aadc88e0638c4ccf37bd17b69d5d5c
bitec <bitec@insecure.com>
2019-07-17 15:25:22 +0900

    test Sin gi han

--- SimpleChatProject/src/com/exam/simpleTest/SimpleTest.java
+++ SimpleChatProject/src/com/exam/simpleTest/SimpleTest.java
@@ -3,7 +3,7 @@ package com.exam.simpleTest;
 public class SimpleTest {
 
 	public static void main(String[] args) {
-		System.out.println("Hello world");
+		System.out.println("Hello world sin gi han");
 
 	}
 
