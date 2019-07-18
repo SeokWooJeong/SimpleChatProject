2a8968f3203e47d25843910234e55134a4f23b7b
BIT <BIT@DESKTOP-L>
2019-07-18 09:55:55 +0900

    정석우 UTF-8 인코딩 완료

--- SimpleChatProject/src/com/exam/simpleTest/SimpleTest.java
+++ SimpleChatProject/src/com/exam/simpleTest/SimpleTest.java
@@ -6,19 +6,12 @@ public class SimpleTest {
 		System.out.println("Hello world sin gi han, coo, seok woo");
 		System.out.println("Hello world sin gi han, coo, seok woo");
 		System.out.println("Hello world sin gi han, coo, seok woo");
-<<<<<<< HEAD
-		System.out.println("±âÇÑÂô");
-		System.out.println("±âÇÑÂô");
-=======
+
 		System.out.println("Hello world sin gi han, coo, seok woo");
 		System.out.println("Hello world sin gi han, coo, seok woo");
-<<<<<<< HEAD
->>>>>>> branch 'master' of https://github.com/SeokWooJeong/SimpleChatProject.git
 		System.out.println(456);
-=======
 		System.out.println(123);
->>>>>>> branch 'master' of https://github.com/SeokWooJeong/SimpleChatProject.git
-	System.out.println("ÇÏÁ¤ÈÆ");
+		System.out.println("ì •ì„ìš° UTF-8 ì¸ì½”ë”© ì™„ë£Œ");
 	}
 
 }
