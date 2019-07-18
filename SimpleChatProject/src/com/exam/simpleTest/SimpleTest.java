f40f29b3cbe56e2989dc28df0eed82ca2f0ee542
coo <coo@DESKTOP-UVRQ3DE>
2019-07-18 09:24:48 +0900

    test

--- SimpleChatProject/src/com/exam/simpleTest/SimpleTest.java
+++ SimpleChatProject/src/com/exam/simpleTest/SimpleTest.java
@@ -5,6 +5,7 @@ public class SimpleTest {
 	public static void main(String[] args) {
 		System.out.println("Hello world sin gi han, coo, seok woo");
 		System.out.println("Hello world sin gi han, coo, seok woo");
+		System.out.println("Hello world sin gi han, coo, seok woo");
 	}
 
 }
